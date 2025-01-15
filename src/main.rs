fe2e2ef39665881ca9ad701763794471812e3499
Clément DOUIN <clement.douin@posteo.net>
2025-01-15 09:48:41 +0100

    take host and port from env

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use cardamum::std::IoConnector;
 
 fn main() {
-    let mut connector = IoConnector::new("localhost", 8001).unwrap();
+    let host = env::var("HOST").unwrap_or(String::from("localhost"));
+    println!("using host: {host:?}");
+
+    let port = env::var("PORT").unwrap_or(String::from("8001"));
+    let port = port.parse::<usize>().expect("should be an integer");
+    println!("using port: {port:?}");
+
+    let mut connector = IoConnector::new(host, port).unwrap();
 
     let contacts = connector
         .list_contacts("6fa928d4-e344-3021-1ad2-c652209ae251")
